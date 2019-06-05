c8682b752c8eeefa7418e2cda9e18481c7f3c9bf
Bilal Elhalawaty <bilal.halawaty@gmail.com>
2019-06-05 20:48:24 +0200

    using Guzzle http client GET request instead of CURL

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -1,34 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Country;
-use App\User;
-use Illuminate\Http\Request;
-use JWTAuth;
+
+//use App\Country;
+//use App\User;
+
 class CountryController extends Controller
 {
     protected $user;
 
-    public function __construct()
-    {
-//        $this->user = JWTAuth::parseToken()->authenticate();
-    }
-
     public function index()
     {
-        //using public api
-        $url="https://api.printful.com/countries";
-        $ch=curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        $output=curl_exec($ch);
-        return $output;
+        //get public api using Guzzle
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('https://api.printful.com/countries');
+        $response = $request->getBody();
+        return $response;
 
-        //fetch data from database
-//        return $this->user
-//            ->countries()
-//            ->get(['name'])
-//            ->toArray();
+        //get public api using curl
+//        $url = "https://api.printful.com/countries";
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        $output = curl_exec($ch);
+//        return $output;
+
 
     }
 }

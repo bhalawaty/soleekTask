4c810da96a1deb9c9551d525ada9306972f0334d
Bilal Elhalawaty <bilal.halawaty@gmail.com>
2019-06-18 10:04:02 +0200

    sorting name of countries by length using usort() function

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -11,12 +11,20 @@ class CountryController extends Controller
 
     public function index()
     {
+
         //get public api using Guzzle
         $client = new \GuzzleHttp\Client();
         $request = $client->get('https://api.printful.com/countries');
         $response = $request->getBody();
-        return $response;
-
-
+        $someArray = json_decode($response, true);
+        $someArray = $someArray['result'];
+        //print_r($someArray);
+        //return $someArray['result'][0]['name'];
+        usort($someArray, function ($a, $b) {
+            $a = strlen($a['name']);
+            $b = strlen($b['name']);
+            return $b - $a;
+        });
+        return $someArray;
     }
 }
